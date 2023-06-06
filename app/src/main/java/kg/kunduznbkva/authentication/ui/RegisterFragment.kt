2e2048216151b3fd2bda8ade7f37aec447b73bdb
kunduz <kunduznbkva@gmail.com>
2023-06-06 21:25:25 +0600

    created new fragments worked on register screen and add custom dialog view

---
+++ app/src/main/java/kg/kunduznbkva/authentication/ui/RegisterFragment.kt
@@ -0,0 +1,22 @@
+package kg.kunduznbkva.authentication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kg.kunduznbkva.authentication.R
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+
+}
