dc014a86074356f37adcba24e77abb4f13a7ce8f
kunduz <kunduznbkva@gmail.com>
2023-06-08 01:19:12 +0600

    add check input email

--- app/src/main/java/kg/kunduznbkva/authentication/data/remote/UserAPI.kt
+++ app/src/main/java/kg/kunduznbkva/authentication/data/remote/UserAPI.kt
@@ -1,6 +1,7 @@
 package kg.kunduznbkva.authentication.data.remote
 
 import kg.kunduznbkva.authentication.data.model.UserCreateRequest
+import kg.kunduznbkva.authentication.data.model.UserEmail
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -8,5 +9,8 @@ interface UserAPI {
 
     @POST("register/")
     fun register(@Body request: UserCreateRequest): UserCreateRequest
+
+    @POST("register/email/")
+    fun registerEmail(@Body request: UserEmail): UserCreateRequest
 
 }
--- app/src/main/java/kg/kunduznbkva/authentication/ui/register/RegisterFragment.kt
+++ app/src/main/java/kg/kunduznbkva/authentication/ui/register/RegisterFragment.kt
@@ -1,12 +1,17 @@
 package kg.kunduznbkva.authentication.ui.register
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import kg.kunduznbkva.authentication.R
 import kg.kunduznbkva.authentication.databinding.FragmentRegisterBinding
+import java.util.*
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
@@ -18,5 +23,43 @@ class RegisterFragment : Fragment() {
         binding = FragmentRegisterBinding.inflate(layoutInflater,container,false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkInputEmail()
+        backBtn()
+    }
+
+    private fun backBtn() {
+        binding.backBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+        }
+    }
+
+    private fun checkInputEmail() {
+        binding.etEmail.addTextChangedListener(inputTxtWatcher)
+    }
+
+    private val inputTxtWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val emailInput = binding.etEmail.text.toString().trim()
+            val button = binding.nextBtn
+
+            if (!emailInput.contains("@")) {
+                button.isEnabled = true
+                button.setBackgroundResource(R.drawable.btn_bg)
+            } else {
+                button.isEnabled = false
+                button.setBackgroundResource(R.drawable.fields_back)
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
+
 
 }
